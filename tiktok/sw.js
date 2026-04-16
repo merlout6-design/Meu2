ecf2675c1eb58fb8b99bffb25a35dffca4b46421
merlout6-design <merlout6@gmail.com>
2026-04-16 19:09:27 -0300

    Create sw.js

---
+++ tiktok/sw.js
@@ -0,0 +1,4 @@
+// sw.js - Service Worker básico para permitir a instalação do PWA
+self.addEventListener('fetch', function(event) {
+    // Permanece vazio para o PWA básico, serve apenas para ativar o aviso de instalação no navegador
+});
